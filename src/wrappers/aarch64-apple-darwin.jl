b861d6aae93d47a55fab237c53c5a96c94bb7a83
jlbuild <juliabuildbot@gmail.com>
2023-02-27 09:59:08 +0000

    MbedTLS_jll build 2.28.2+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -9,7 +9,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libmbedcrypto,
-        "lib/libmbedcrypto.2.28.0.dylib",
+        "lib/libmbedcrypto.2.28.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
