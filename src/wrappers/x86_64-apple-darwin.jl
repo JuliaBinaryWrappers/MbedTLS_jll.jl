20eb4a11aeeff935cd10226fceb4b9a40a84b8a3
jlbuild <juliabuildbot@gmail.com>
2024-12-26 19:39:47 +0000

    MbedTLS_jll build 2.24.0+5

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -2,20 +2,20 @@
 export libmbedcrypto, libmbedtls, libmbedx509
 
 JLLWrappers.@generate_wrapper_header("MbedTLS")
-JLLWrappers.@declare_library_product(libmbedcrypto, "@rpath/libmbedcrypto.7.dylib")
-JLLWrappers.@declare_library_product(libmbedtls, "@rpath/libmbedtls.14.dylib")
+JLLWrappers.@declare_library_product(libmbedcrypto, "@rpath/libmbedcrypto.5.dylib")
+JLLWrappers.@declare_library_product(libmbedtls, "@rpath/libmbedtls.13.dylib")
 JLLWrappers.@declare_library_product(libmbedx509, "@rpath/libmbedx509.1.dylib")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libmbedcrypto,
-        "lib/libmbedcrypto.2.28.6.dylib",
+        "lib/libmbedcrypto.2.24.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libmbedtls,
-        "lib/libmbedtls.14.dylib",
+        "lib/libmbedtls.13.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
